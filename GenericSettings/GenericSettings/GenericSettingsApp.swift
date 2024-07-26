67d0040a8092729545b6dc02a837faaffc798f7f
CarolaneLFBV <carolane_lefebvre@icloud.com>
2024-07-26 17:45:48 +0200

    feat: initial commit

---
+++ GenericSettings/GenericSettings/GenericSettingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GenericSettingsApp.swift
+//  GenericSettings
+//
+//  Created by Carolane Lefebvre on 26/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct GenericSettingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
